b1ddd2ec3ebb0771b32bfe04d86452abf04e412c
Roman <ezgg2000@gmail.com>
2024-09-20 18:35:00 +0300

    Just play with fs from rust perspective

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,24 @@ use std::{
     error::Error, 
     ffi::{OsStr, OsString}, 
     fs::read_dir,
-    process,
 };
 
 fn main() -> Result<(), Box<dyn Error>>{
     let mut args = std::env::args_os();
     let mut target_dir = OsString::from(r".\");
 
-    let bin_name = args.next().unwrap_or_else(|| {
+    let _bin_name = args.next().unwrap_or_else(|| {
         eprintln!("Could not get bin name");
         std::process::exit(1);
     });
 
-    println!("{bin_name:?}");
+    let max_depth = 3;
 
     if let Some(t) = args.next(){
         target_dir = t.clone();
     }
     
-    let _ = dir_travel(&target_dir)?;
+    let _ = dir_travel(&target_dir, 0, max_depth)?;
 
     
 
@@ -28,28 +27,33 @@ fn main() -> Result<(), Box<dyn Error>>{
 }
 
 
-fn dir_travel(path: &OsStr) -> Result<(), Box<dyn Error>>{
+fn dir_travel(path: &OsStr, depth: usize, max_depth: usize) -> Result<(), Box<dyn Error>>{
+
+    if max_depth <= 0{
+        return Ok(());
+    }
 
     let dir_reader = read_dir(path)?;
     for dir_entry in dir_reader{
 
-        let dir_entry = dir_entry.unwrap_or_else(|err| {
-            eprintln!("{:?}", err.kind());
-            process::exit(1);
-        });
+        if let Ok(entry) = dir_entry{
 
-        match dir_entry.file_type(){
-            Ok(i) => {
-                
-                if i.is_dir(){
-                    println!("Dirname: {:?}", dir_entry.file_name());
-                    dir_travel(dir_entry.path().as_os_str())?
+            let entry_type = entry.file_type();
+            let entry_path = entry.path();
+            let entry_name = entry.file_name();
+            if let Ok(t) = entry_type{
+
+                if t.is_dir(){
+                    println!("D: {}{:?}:", "\t".repeat(depth), entry_name);
+                    let _ = dir_travel(entry_path.as_os_str(), depth + 1, max_depth - 1);
                 }else{
-                    println!("Filename: {:?}", dir_entry.file_name());
+                    println!("F: {}{:?}", "\t".repeat(depth), entry_name);
                 }
-            },
-            Err(e) => return Err(Box::new(e)),
+            }
+        }else{
+            eprintln!("[-] Could not operate with entry.")
         }
+        
     }
     Ok(())
 }
