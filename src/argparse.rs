6fe4613dd52790c887fc7b57e8ee3c715f62aa1a
Roman <ezgg2000@gmail.com>
2024-09-20 19:42:33 +0300

    added cli flag system using clap crate

---
+++ src/argparse.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+use std::ffi::OsString;
+
+#[derive(Parser)]
+#[command(name="rtree")]
+#[command(name="1.0")]
+#[command(about="Rusty version of tree unix tool", long_about=None)]
+pub struct Args{
+    #[arg(default_value = ".")]
+    pub name: Option<OsString>,
+    #[arg(short, long, default_value_t = 2)]
+    pub depth: usize,
+    #[arg(short, long,)]
+    pub all: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use std::{
     error::Error, 
-    ffi::{OsStr, OsString}, 
+    ffi::OsStr, 
     fs::read_dir,
 };
+use clap::Parser;
+
+mod argparse;
+
+use argparse::Args;
+
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let mut args = std::env::args_os();
-    let mut target_dir = OsString::from(r".\");
 
-    let _bin_name = args.next().unwrap_or_else(|| {
-        eprintln!("Could not get bin name");
-        std::process::exit(1);
-    });
+    let args = Args::parse();
+ 
+    let target_dir = args.name.unwrap();
+    let print_all = args.all;
 
-    let max_depth = 3;
+    let max_depth = args.depth;
 
-    if let Some(t) = args.next(){
-        target_dir = t.clone();
-    }
-    
-    let _ = dir_travel(&target_dir, 0, max_depth)?;
-
-    
+    let _ = dir_travel(&target_dir, 0, max_depth, print_all)?;
 
     Ok(())
 }
 
 
-fn dir_travel(path: &OsStr, depth: usize, max_depth: usize) -> Result<(), Box<dyn Error>>{
+fn dir_travel(path: &OsStr, depth: usize, max_depth: usize, print_all: bool) -> Result<(), Box<dyn Error>>{
 
     if max_depth <= 0{
         return Ok(());
@@ -41,17 +39,29 @@ fn dir_travel(path: &OsStr, depth: usize, max_depth: usize) -> Result<(), Box<dy
             let entry_type = entry.file_type();
             let entry_path = entry.path();
             let entry_name = entry.file_name();
+            // Check if the file name starts with a dot symbol (according to the unix it is hidden file)
+            if entry_name.as_encoded_bytes()[0] == b'.' && !print_all{
+                continue;
+            }
+
             if let Ok(t) = entry_type{
 
                 if t.is_dir(){
                     println!("D: {}{:?}:", "\t".repeat(depth), entry_name);
-                    let _ = dir_travel(entry_path.as_os_str(), depth + 1, max_depth - 1);
+                    let _ = dir_travel(
+                        entry_path.as_os_str(), 
+                        depth + 1, 
+                        max_depth - 1,
+                        print_all
+                    );
+
                 }else{
                     println!("F: {}{:?}", "\t".repeat(depth), entry_name);
                 }
+
             }
         }else{
-            eprintln!("[-] Could not operate with entry.")
+            eprintln!("[-] Could not operate on entry.")
         }
         
     }
